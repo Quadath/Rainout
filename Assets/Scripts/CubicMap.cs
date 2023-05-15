8005ee0f0b94f689f4e62ad445934cdcfcb8633a
Quadatz <quadath@gmail.com>
2023-05-15 19:00:51 +0300

    awesome mesh generator

--- Assets/Scripts/CubicMap.cs
+++ Assets/Scripts/CubicMap.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Random = UnityEngine.Random;
 
 public class CubicMap : MonoBehaviour
 {
-    private int xSize = 8;
-    private int ySize = 16;
-    private int zSize = 8;
+    private int xSize = 128;
+    private int ySize = 128;
+    private int zSize = 128;
 
     private Mesh mesh;
 
@@ -17,6 +18,8 @@ public class CubicMap : MonoBehaviour
     private Vector3[] vertices;
     private Color[] colors;
     private int[] triangles;
+
+    private int[,] heights;
 
     private int vert, tris;
 
@@ -30,6 +33,7 @@ public class CubicMap : MonoBehaviour
     private void Start()
     {
         mesh = new Mesh();
+        mesh.indexFormat = IndexFormat.UInt32;
         GetComponent<MeshFilter>().mesh = mesh;
         
 
@@ -41,9 +45,13 @@ public class CubicMap : MonoBehaviour
             {
                 for (int z = 0; z < xSize; z++)
                 {
-                    if (x == 0) tiles[x, y, z] = true;
-                    if (y == 0) tiles[x, y, z] = true;
-                    if (z == 0) tiles[x, y, z] = true;
+                    if (x is > 2 and < 5)
+                    {
+                        if (y is > 2 and < 5)
+                        {
+                            // if (z is > 2 and < 5) tiles[x, y, z] = true;
+                        }
+                    }
                 }
             }
         }
@@ -55,8 +63,10 @@ public class CubicMap : MonoBehaviour
     void CreateShape()
     {
         vertices = new Vector3[(xSize + 1) * (ySize + 1) * (zSize + 1)];
-        colors = new Color[(xSize + 1) * (ySize + 1) * (zSize + 1)];
         triangles = new int[xSize * zSize * ySize * 6];
+        colors = new Color[(xSize + 1) * (ySize + 1) * (zSize + 1)];
+
+        heights = new int[xSize, zSize];
 
         int v = 0;
         for (int y = 0; y <= ySize; y++)
@@ -66,30 +76,38 @@ public class CubicMap : MonoBehaviour
                 for (int x = 0; x <= xSize; x++)
                 {
                     vertices[v] = new Vector3(x, y, z);
+                    colors[v] = Color.Lerp(Color.black, new Color(0.97f, 0.79f, 0.5f), (y - 10) / 9.5f);
                     v++;
                 }
             }
         }
 
-        StartCoroutine(processing());
+        for (int y = 0; y < ySize; y++)
+        {
+            for (int z = 0; z < zSize; z++)
+            {
+                for (int x = 0; x < xSize; x++)
+                {
+                    if (y == 0)
+                        heights[x, z] = (int) (Mathf.PerlinNoise(x * .04f, z * .04f) * 15f + 10);
+                    if (y < heights[x, z]) tiles[x, y, z] = true;
+                }
+            }
+        }
 
-        // int vert = 0;
-        // int tris = 0;
-        // for (int z = 0; z < zSize; z++)
-        // {
-        //     for (int x = 0; x < xSize; x++)
-        //     {
-        //         triangles[tris + 0] = vert + 0;
-        //         triangles[tris + 1] = vert + xSize + 1;
-        //         triangles[tris + 2] = vert + 1;
-        //         triangles[tris + 3] = vert + 1;
-        //         triangles[tris + 4] = vert + xSize + 1;
-        //         triangles[tris + 5] = vert + xSize + 2;
-        //         vert++;
-        //         tris += 6;
-        //     }
-        //     vert++;
-        // }
+        for (int y = 0; y < ySize; y++)
+        {
+            UpdateMesh();
+            for (int z = 0; z < xSize; z++)
+            {
+                for (int x = 0; x < xSize; x++)
+                {
+                    ProcessTile(new Vector3Int(x, y, z));
+                }
+            }
+        }
+        
+        // StartCoroutine(processing());
     }
 
     IEnumerator processing()
@@ -120,72 +138,102 @@ public class CubicMap : MonoBehaviour
                 break;
             }
 
-            yield return new WaitForSeconds(0.25f);
+            yield return new WaitForEndOfFrame();
         }
     }
     void ProcessTile(Vector3Int pos)
     {
         if (!tiles[pos.x, pos.y, pos.z]) return;
+        
         if (pos.x == 0)
         {
-            CreateFace(pos, Planes.X, true);
+            CreateFace(pos, Planes.X, false);
         }
         if (pos.y == 0)
         {
             CreateFace(pos, Planes.Y, false);
         }
-
         if (pos.z == 0)
         {
             CreateFace(pos, Planes.Z, false);
         }
+        if (pos.x < xSize - 1 && !tiles[pos.x + 1, pos.y, pos.z]) //FORWARD
+            CreateFace(pos + Vector3Int.right, Planes.X, true);
+        if (pos.x > 0 && !tiles[pos.x - 1, pos.y, pos.z]) //BACK
+            CreateFace(pos, Planes.X, false);
+        if (pos.y < ySize - 1 && !tiles[pos.x, pos.y + 1, pos.z]) //UP
+            CreateFace(pos + Vector3Int.up, Planes.Y, true);
+        if (pos.y > 0 && !tiles[pos.x, pos.y - 1, pos.z]) //DOWN
+            CreateFace(pos, Planes.Y, false);
+        if(pos.z < zSize - 1 && !tiles[pos.x, pos.y, pos.z + 1]) //RIGHT
+            CreateFace(pos + Vector3Int.forward, Planes.Z, true);
+        if (pos.z > 0 && !tiles[pos.x, pos.y, pos.z - 1]) //LEFT
+            CreateFace(pos, Planes.Z, false);
+        
+        if (pos.x == xSize - 1) //END X
+            CreateFace(pos + Vector3Int.right, Planes.X, true);
+        if (pos.y == ySize - 1) //END Y
+            CreateFace(pos + Vector3Int.up, Planes.Y, true);
+        if (pos.z == zSize - 1) //END Z
+            CreateFace(pos + Vector3Int.forward, Planes.Z, true);
     }
     void CreateFace(Vector3Int p, Planes plane, bool inside)
     {
-        int corner = 0;
+        int corner;
         switch (plane)
         {
             case Planes.X:
             {
-                corner = p.y * (xSize + 1) * (zSize + 1) + p.z * (xSize + 1);
-                
-                triangles[inside ? tris + 2 : tris] = corner;
-                triangles[tris + 1] = corner + xSize + 1;
-                triangles[inside? tris : tris + 2] = corner + (xSize + 1) * (zSize + 1) + (xSize + 1);
-                triangles[inside ? tris + 5 : tris + 3] = corner + (xSize + 1) * (zSize + 1) + (xSize + 1);
-                triangles[tris + 4] = corner + (xSize + 1) * (zSize + 1);
-                triangles[inside? tris + 3 : tris + 5] = corner;
-
-                tris += 6;
+                corner = p.y * (xSize + 1) * (zSize + 1) + p.z * (xSize + 1) + p.x;
+                DefineTriangles(new []
+                {
+                    corner,
+                    corner + xSize + 1,
+                    corner + (xSize + 1) * (zSize + 1) + (xSize + 1),
+                    corner + (xSize + 1) * (zSize + 1) + (xSize + 1),
+                    corner + (xSize + 1) * (zSize + 1),
+                    corner
+                }, inside);
             } break;
             case Planes.Y:
             {
-                corner = p.z * (xSize + 1) + p.x;
-                
-                triangles[tris] = corner;
-                triangles[tris + 1] = corner + xSize + 1;
-                triangles[tris + 2] = corner + xSize + 2;
-                triangles[tris + 3] = corner + xSize + 2;
-                triangles[tris + 4] = corner + 1;
-                triangles[tris + 5] = corner;
-
-                tris += 6;
+                corner = p.y * (xSize + 1) * (zSize + 1) + p.z * (xSize + 1) + p.x;
+                DefineTriangles(new []
+                {
+                    corner,
+                    corner + xSize + 2,
+                    corner + xSize + 1,
+                    corner,
+                    corner + 1,
+                    corner + xSize + 2
+                }, inside);
             } break;
             case Planes.Z:
             {
-                corner = p.y * (xSize + 1) * (zSize + 1) + p.x;
-                
-                triangles[tris] = corner;
-                triangles[tris + 1] = corner + (xSize + 1) * (zSize + 1);
-                triangles[tris + 2] = corner + (xSize + 1) * (zSize + 1) + 1;
-                triangles[tris + 3] = corner;
-                triangles[tris + 4] = corner + (xSize + 1) * (zSize + 1) + 1;
-                triangles[tris + 5] = corner + 1;
-
-                tris += 6;
+                corner = p.y * (xSize + 1) * (zSize + 1) + p.z * (xSize + 1) + p.x;
+                DefineTriangles(new []
+                {
+                    corner,
+                    corner + (xSize + 1) * (zSize + 1),
+                    corner + (xSize + 1) * (zSize + 1) + 1,
+                    corner,
+                    corner + (xSize + 1) * (zSize + 1) + 1,
+                    corner + 1
+                }, inside);
             } break;
         }
-        UpdateMesh();
+    }
+
+    private void DefineTriangles(int[] tri, bool inside)
+    {
+        triangles[inside ? tris + 2 : tris] = tri[0];
+        triangles[tris + 1] = tri[1];
+        triangles[inside ? tris : tris + 2] = tri[2];
+        triangles[inside ? tris + 5 : tris + 3] = tri[3];
+        triangles[tris + 4] = tri[4];
+        triangles[inside ? tris + 3 : tris + 5] = tri[5];
+        
+        tris += 6;
     }
     void UpdateMesh()
     {
@@ -193,7 +241,7 @@ public class CubicMap : MonoBehaviour
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-        // mesh.colors = colors;
+        mesh.colors = colors;
         // GetComponent<MeshCollider>().sharedMesh = mesh;
         mesh.RecalculateNormals();
     }
